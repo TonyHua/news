cf8abb078b0b62567f53108e2291bba21e384cad
TonyHua <bravefly1@163.com>
2014-08-18 17:47:32 +0800

    first commmit

---
+++ src/com/views/NewsMainActivity.java
@@ -0,0 +1,18 @@
+package com.views;
+
+
+import com.phone.news.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewsMainActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.newsmain);
+	}
+
+}
